16da0f4f091863668f71a5b9fbf7dda85383268e
Kemboi <79087047+okemboi@users.noreply.github.com>
2023-08-03 09:22:28 +0300

    Update script.js
    I updated the JavaScript file with a function

--- script.js
+++ script.js
@@ -26,7 +26,19 @@ function myFunction() {
   });
 
   close.addEventListener('click', () => {
-
+    navLi.forEach(link => link.classList.toggle('nav-li-navigation'));
+    body.classList.toggle('body-navigation');
+    toolbar.classList.toggle('toolbar-navigation');
+    navUl.classList.remove('nav-links-navigation')
+    navUl.classList.add('nav-links');
+    burger.classList.remove('hamburger-navigation');
+    burger.classList.add('hamburger');
+    close.classList.remove('close-navigation');
+    close.classList.add('close');
+    logo.classList.remove('logo-navigation');
+    logo.classList.add('logo');
+    navToolbar.classList.remove('nav_toolbar_navigation');
+    navToolbar.classList.add('nav_toolbar');
   });
 }
 
