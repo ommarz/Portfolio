1f9f040ae46b189159e1a89360ba68b0263fa5a1
Kemboi <79087047+okemboi@users.noreply.github.com>
2023-08-05 20:24:41 +0300

    Update script.js
    Added windows pop

--- script.js
+++ script.js
@@ -60,3 +60,244 @@ function myFunction() {
 }
 
 myFunction();
+
+//POP UP CARDS PROJECT
+
+  //Create array of projects
+
+  const popUpProjects = [
+    {
+      name: "Multi-Post Stories",
+      toolsUsed: ["HTML", "CSS", "JavaScript"],
+      image: "images/content/Snapshoot_Portfolio.svg",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+      seeLive: "#",
+      seeSource: "#"
+    },
+
+    {
+      name: "Keeping track of hundreds of components website",
+      toolsUsed: ["HTML", "Bootstrap", "Ruby on Rails"],
+      image: "images/content/Snapshoot_Portfolio.svg",
+      description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500"],
+      seeLive: '#',
+      seeSource: '#'
+    },
+
+    {
+      name: "Data Dashboard",
+      toolsUsed: ["HTML", "Bootstrap", "Ruby"],
+      image: "images/content/Snapshoot_Portfolio.svg",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since",
+      seeLive: '#',
+      seeSource: '#'
+    },
+
+    {
+      name: "Website Portfolio",
+      toolsUsed: ["HTML", "Bootstrap", "Ruby"],
+      image: "images/content/Snapshoot_Portfolio.svg",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since",
+      seeLive: '#',
+      seeSource: '#'
+    },
+
+    {
+      name: "Professional Art",
+      toolsUsed: ["HTML", "Bootstrap", "Ruby"],
+      image: "images/content/Snapshoot_Portfolio.svg",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since",
+      seeLive: '#',
+      seeSource: '#'
+    },
+
+    {
+        name: "Data Dashboard",
+      toolsUsed: ["HTML", "Bootstrap", "Ruby"],
+      image: "images/content/Snapshoot_Portfolio.svg",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since",
+      seeLive: '#',
+      seeSource: '#'
+    },
+
+    {
+      name: "Website Portfolio",
+      toolsUsed: ["HTML", "Bootstrap", "Ruby"],
+      image: "images/content/Snapshoot_Portfolio.svg",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since",
+      seeLive: '#',
+      seeSource: '#'
+    },
+  ]
+
+  for (let i = 0; i < popUpProjects.length; i++) {
+    // Select the projects div
+      
+    const projectsDiv = document.querySelector('.projects');  
+      
+    // Create the project card
+    
+    const popUpCard = document.createElement('div');
+    popUpCard.setAttribute = ('id','popup-card');
+    popUpCard.classList.add('popUpCard');
+    projectsDiv.appendChild(popUpCard);
+  
+    // Create the project card header
+    const popUpHeader = document.createElement('div');
+    popUpHeader.classList.add('popUpHeader');
+    popUpCard.appendChild(popUpHeader);
+
+    // Create the project card header title
+    const popUpTitle = document.createElement('h3');
+    popUpTitle.classList.add('popUpTitle');
+    popUpTitle.textContent = popUpProjects[i].name;  
+    popUpHeader.appendChild(popUpTitle);
+
+    // Create the project card header close button
+    const popUpClose = document.createElement('img');
+    popUpClose.classList.add('popUpClose');
+    popUpClose.setAttribute = ('src', 'images/icons/Close_Window_icon.svg');
+    popUpHeader.appendChild(popUpClose);
+
+    //Create the project tools used list
+    const popUpToolsUsedList = document.createElement('ul');
+    popUpToolsUsedList.classList.add('popUp-ToolsUsed-list');
+    popUpHeader.appendChild(popUpToolsUsedList);
+
+    // Create the project card tools used elements
+    
+    for(let j = 0; j < popUpProjects[j].toolsUsed.length; j++){
+      const popUpToolsUsedElement = document.createElement('li');
+      popUpToolsUsedElement.classList.add('popUp-ToolsUsed-element');
+      popUpToolsUsedElement.innerHTML = popUpProjects[i].toolsUsed[j];
+      popUpToolsUsedList.appendChild(popUpToolsUsedElement);
+    }
+
+    // Create the project card body
+    const popUpBody = document.createElement('div');
+    popUpBody.classList.add('popUpBody');
+    popUpCard.appendChild(popUpBody);
+    
+    // Create the project card body image
+    const popUpImage = document.createElement('img');
+    popUpImage.classList.add('popup-image');
+    popUpImage.setAttribute =('src', popUpProjects[i].image);
+    popUpImage.setAttribute =('alt', popUpProjects[i].name);
+    popUpBody.appendChild(popUpImage);
+
+    // Create the project card body description
+    const popUpDescription = document.createElement('p');
+    popUpDescription.classList.add('popup-description');
+    popUpDescription.textContent = popUpProjects[i].description;
+    popUpBody.appendChild(popUpDescription);
+
+    // Create the project card body buttons
+    const popUpButtons = document.createElement('div');
+    popUpButtons.classList.add('popUp-buttons');
+    popUpBody.appendChild(popUpButtons);
+
+    // Create the project card body buttons live button
+    const liveButton = document.createElement('a');
+    liveButton.classList.add('live-button');
+    liveButton.setAttribute = ('href', popUpProjects[i].seeLive);
+    popUpButtons.appendChild(liveButton);
+
+    // Create the project card body buttons source button
+    const sourceButton = document.createElement('a');
+    sourceButton.classList.add('source-button');
+    sourceButton.setAttribute =('href', popUpProjects[i].seeSource);
+    popUpButtons.appendChild(sourceButton);
+
+    console.log("card " + i + " created: " + popUpProjects[i].name);
+  }
+
+  // Function to create the project cards
+
+ function createProjectCard(index) {
+  const project = popUpProjects[index];
+
+  const popUpCard = document.createElement('div');  
+  popUpCard.id = 'popup-card';
+  popUpCard.classList.add('popUpCard');
+
+  const popUpHeader = document.createElement('div');
+  popUpHeader.classList.add('popUpHeader');
+  popUpCard.appendChild(popUpHeader);
+
+  const popUpTitle = document.createElement('h3');
+  popUpTitle.classList.add('popUpTitle');
+  popUpTitle.textContent = project.name;
+  popUpHeader.appendChild(popUpTitle);
+
+  const popUpClose = document.createElement('img');
+  popUpClose.classList.add('popUpClose');
+  popUpClose.setAttribute =('src', 'images/icons/Close_Window_icon.svg');
+  popUpHeader.appendChild(popUpClose);
+
+  const popUpToolsUsedList = document.createElement('ul');
+  popUpToolsUsedList.classList.add('popUp-ToolsUsed-list');
+  popUpHeader.appendChild(popUpToolsUsedList);
+  
+  for(let j = 0; j < project.toolsUsed.length; j++){
+    const popUpToolsUsedElement = document.createElement('li');
+    popUpToolsUsedElement.classList.add('popUp-ToolsUsed-element');
+    popUpToolsUsedElement.innerHTML = project.toolsUsed[j];
+    popUpToolsUsedList.appendChild(popUpToolsUsedElement);
+  }
+
+  const popUpBody = document.createElement('div');
+  popUpBody.classList.add('popUpBody');
+  popUpCard.appendChild(popUpBody);
+  
+  const popUpImage = document.createElement('img');
+  popUpImage.classList.add('popup-image');
+  popUpImage.setAttribute = ('src', project.image);
+  popUpBody.appendChild(popUpImage);
+    
+  const popUpDescription = document.createElement('p');
+  popUpDescription.classList.add('popup-description');
+  popUpDescription.textContent = project.description;
+  popUpBody.appendChild(popUpDescription);
+
+  const popUpButtons = document.createElement('div');
+  popUpButtons.classList.add('popUp-buttons');
+  popUpBody.appendChild(popUpButtons);
+    
+  const liveButton = document.createElement('a');
+  liveButton.classList.add('live-button');
+  liveButton.setAttribute = ('href', project.seeLive);
+  popUpButtons.appendChild(liveButton);
+
+  const sourceButton = document.createElement('a');
+  sourceButton.classList.add('source-button');
+  sourceButton.setAttribute = ('href', project.seeSource);
+  popUpButtons.appendChild(sourceButton);
+
+  popUpClose.addEventListener('click', () => {
+    popUpCard.style.display = 'none';
+  });
+
+  liveButton.addEventListener('click', () => {
+    window.open(project.seeLive);
+  });
+
+  sourceButton.addEventListener('click', () => {
+    window.open(project.seeSource);
+  });
+  
+  document.body.appendChild(popUpCard);
+ }
+ 
+ document.addEventListener('DOMContentLoaded', () => {
+  const seeProjectLinks = document.querySelectorAll('.see_project_card');
+
+  seeProjectLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const index = link.getAttribute('data-index');
+      createProjectCard(index);
+
+      const popUpCard = document.querySelectorAll('.popUpCard')[index];
+      console.log(popUpCard.classList);
+      popUpCard.style.display = 'block';
+    });
